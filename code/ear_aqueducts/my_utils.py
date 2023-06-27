abb6ae1c3a7ed9be07181dee8a2eafcaf37bfced
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-06-27 11:28:12 +0100

    update legend

--- code/ear_aqueducts/my_utils.py
+++ code/ear_aqueducts/my_utils.py
@@ -5,7 +5,7 @@ def plot_time_series(times, locations, data):
 
     # Plot data
     color = ['r', 'g', 'b', 'k', 'm']
-    legends = ['loc = '+str(obs) for obs in locations]
+    legends = ['loc = '+"{:.2f}".format(obs) for obs in locations]
     lines = []
     for i in range(len(locations)):
         lines.append(plt.plot(times/60, data[i,:],  color=color[i])[0])
