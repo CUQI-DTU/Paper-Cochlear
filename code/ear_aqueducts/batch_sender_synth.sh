86efd7dd5ed72b8fd77f1838e2d06bcf6ecdc77e
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-08-02 19:13:43 +0200

    new sender scripts for synth expr

---
+++ code/ear_aqueducts/batch_sender_synth.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+./sender_synth.py 'm1' 'l' 'v_aug2_b' 'MH' 'constant' 400 'synthetic' 'both' 100 100 0.1
+./sender_synth.py 'm1' 'l' 'v_aug2_b' 'MH' 'constant' 1000 'synthetic' 'both' 100 100 0.1
