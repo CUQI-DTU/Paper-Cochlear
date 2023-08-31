30cc69ae07bd37b08a530c8b739d19c63939e150
amal-ghamdi <amal.m.alghamdi@gmail.com>
2023-08-31 11:56:56 +0200

    update running parameters

--- code/ear_aqueducts/batch_sender_synth.sh
+++ code/ear_aqueducts/batch_sender_synth.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+#./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 100 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 0
+#./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'smooth' -unknown_par_value 400 1200 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 
 
-./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 100 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 0
-./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'smooth' -unknown_par_value 400 1200 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 
+#./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'smooth' -unknown_par_value 400 1200 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 -add_data_pts 480
+#./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 100 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 -add_data_pts 480
 
-
-./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'smooth' -unknown_par_value 400 1200 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 -add_data_pts 480
-./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 100 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.01 -add_data_pts 480
-
-./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'smooth' -unknown_par_value 400 1200 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.1 -add_data_pts 480
-./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 100 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.1 -add_data_pts 480
-
+./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun_aug31_rbn' -sampler 'NUTS' -unknown_par_type 'smooth' -unknown_par_value 400 1200 -data_type 'synthetic' -inference_type 'both' -Ns_const 50 -Ns_var 50 -noise_level 0.1 -add_data_pts 480
+#./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug25_a_rerun' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 100 -data_type 'synthetic' -inference_type 'both' -Ns_const 5000 -Ns_var 5000 -noise_level 0.1 -add_data_pts 480
 
 #./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug2_c' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 400 -data_type 'synthetic' -inference_type 'both' -Ns_const 1000 -Ns_var 1000 -noise_level 0.1 0
 #./sender_synth.sh -animal 'm1' -ear 'l' -version 'v_aug2_c' -sampler 'NUTS' -unknown_par_type 'constant' -unknown_par_value 100 -data_type 'synthetic' -inference_type 'both' -Ns_const 1000 -Ns_var 1000 -noise_level 0.1 0
