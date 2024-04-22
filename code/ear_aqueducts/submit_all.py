80e9d5bf8646969417d1966d9beb9eab7b58aff3
amal-ghamdi <amal.m.alghamdi@gmail.com>
2024-04-22 05:42:31 +0200

    Submit 160 jobs with NUTS 1000 samples

--- code/ear_aqueducts/submit_all.py
+++ code/ear_aqueducts/submit_all.py
@@ -11,12 +11,12 @@ from advection_diffusion_inference_utils import all_animals, all_ears, Args,\
 animals = all_animals()
 # Array of all ears
 ears = all_ears()
-noise_levels = ["from_data_var", "from_data_avg"]
-num_ST_list = [0, 8]
-version = 'v_April20_2024'
-sampler = 'MH'
-Ns = 20
-Nb = 10
+noise_levels = ["from_data_var", "from_data_avg", 0.1, 0.2]
+num_ST_list = [0, 4]
+version = 'v_April22_2024_a'
+sampler = 'NUTS'
+Ns = 1000
+Nb = 20
 
 # Main command to run the job
 main_command = "python advection_diffusion_inference.py"
