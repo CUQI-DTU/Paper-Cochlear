dd5233d7b5ecfb6978c3aea358b9de1e36f49c45
amal-ghamdi <amal.m.alghamdi@gmail.com>
2024-05-06 05:47:50 +0200

    Add new cases May 6

--- code/ear_aqueducts/submit_all.py
+++ code/ear_aqueducts/submit_all.py
@@ -1,4 +1,4 @@
-# Submit all the jobs to the cluster
+#%% Submit all the jobs to the cluster
 # Usage: python3 submit_all.py
 
 # Importing necessary libraries
@@ -6,17 +6,47 @@ import os
 from job_submit import submit, create_command
 from advection_diffusion_inference_utils import all_animals, all_ears, Args,\
     create_experiment_tag
+version = 'v06May2024_a'
 
-# Array of all animals
-animals = all_animals()
-# Array of all ears
-ears = all_ears()
-noise_levels = ["from_data_var", "from_data_avg", 0.1, 0.2]
-num_ST_list = [0, 4]
-version = 'v_April22_2024_'
-sampler = 'NUTS'
-Ns = 20
-Nb = 10
+if version == 'v_April22_2024_':
+    # Array of all animals
+    animals = all_animals()
+    # Array of all ears
+    ears = all_ears()
+    noise_levels = ["fromDataVar", "fromDataAvg", 0.1, 0.2]
+    num_ST_list = [0, 4]
+    version = 'v_April22_2024_'
+    sampler = 'NUTS'
+    Ns = 20
+    Nb = 10
+    data_type = 'real'
+    unknown_par_types = ['constant']
+    unknown_par_values = [[100.0]]
+
+elif version == 'v06May2024_a':
+    # Array of all animals
+    animals = [None]
+    # Array of all ears
+    ears = [None]
+    noise_levels = ["fromDataVar", "fromDataAvg", "avgOverTime", 0.1, 0.2]
+    num_ST_list = [4]
+    version = 'v06May2024_a'
+    sampler = 'NUTS'
+    Ns = 20
+    Nb = 10
+    data_type = 'syntheticFromDiffusion'
+    unknown_par_types = ['sampleMean']
+    unknown_par_values = ['m1:l:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm1:r:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm2:l:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm2:r:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm3:l:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm3:r:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm4:l:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm4:r:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm6:l:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output',
+                          'm6:r:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4/output']
+    
 
 # Main command to run the job
 main_command = "python advection_diffusion_inference.py"
@@ -26,19 +56,25 @@ for animal in animals:
     for ear in ears:
         for noise_level in noise_levels:
             for num_ST in num_ST_list:
-                args = Args()
-                args.animal = animal
-                args.ear = ear
-                args.version = version
-                args.sampler = sampler
-                args.data_type = 'real'
-                args.Ns = Ns
-                args.Nb = Nb
-                args.noise_level = noise_level
-                args.num_ST = num_ST
-                args.inference_type = 'heterogeneous'
+                for unknown_par_type in unknown_par_types:
+                    for unknown_par_value in unknown_par_values:
+                        args = Args()
+                        args.animal = animal if animal is not None else unknown_par_value.split(':')[0]
+                        args.ear = ear if ear is not None else unknown_par_value.split(':')[1]
+                        args.version = version
+                        args.sampler = sampler
+                        args.data_type = data_type
+                        args.Ns = Ns
+                        args.Nb = Nb
+                        args.noise_level = noise_level
+                        args.num_ST = num_ST
+                        args.inference_type = 'heterogeneous'
+                        args.unknown_par_type = unknown_par_type
+                        args.unknown_par_value = unknown_par_value
 
-                cmd = create_command(main_command, args)
-                print(cmd)
-                tag = create_experiment_tag(args)
-                submit(tag, cmd)
+                        cmd = create_command(main_command, args)
+                        print(cmd)
+                        print()
+                        tag = create_experiment_tag(args)
+                        print(tag)
+                        submit(tag, cmd)
