c12f11ba6c6df90d0366192399ac656c677152be
amal-ghamdi <amal.m.alghamdi@gmail.com>
2024-05-07 06:15:41 +0200

    refactor

--- code/ear_aqueducts/submit_all.py
+++ code/ear_aqueducts/submit_all.py
@@ -5,7 +5,7 @@
 import os
 from job_submit import submit, create_command
 from advection_diffusion_inference_utils import all_animals, all_ears, Args,\
-    create_experiment_tag
+    create_experiment_tag, create_args_list
 version = 'v06May2024_a'
 Ns = 20
 Nb = 10
@@ -44,35 +44,14 @@ elif version == 'v06May2024_a':
                           'm4:r:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4',
                           'm6:l:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4',
                           'm6:r:NUTS:constant:100.0:real:heterogeneous:1000:0.1:v:April22:2024:a::4:5@results4']
-    
 
 # Main command to run the job
 main_command = "python advection_diffusion_inference.py"
-
-# Loop over all animals, ears, noise levels and num_ST
-for animal in animals:
-    for ear in ears:
-        for noise_level in noise_levels:
-            for num_ST in num_ST_list:
-                for unknown_par_type in unknown_par_types:
-                    for unknown_par_value in unknown_par_values:
-                        args = Args()
-                        args.animal = animal if animal is not None else unknown_par_value.split(':')[0]
-                        args.ear = ear if ear is not None else unknown_par_value.split(':')[1]
-                        args.version = version
-                        args.sampler = sampler
-                        args.data_type = data_type
-                        args.Ns = Ns
-                        args.Nb = Nb
-                        args.noise_level = noise_level
-                        args.num_ST = num_ST
-                        args.inference_type = 'heterogeneous'
-                        args.unknown_par_type = unknown_par_type
-                        args.unknown_par_value = unknown_par_value
-
-                        cmd = create_command(main_command, args)
-                        print(cmd)
-                        print()
-                        tag = create_experiment_tag(args)
-                        print(tag)
-                        submit(tag, cmd)
+arg_list = create_args_list(animals, ears, noise_levels, num_ST_list, unknown_par_types, unknown_par_values, data_type, version, sampler, Ns, Nb)
+for args in arg_list:
+    cmd = create_command(main_command, args)
+    print()
+    print(cmd)
+    tag = create_experiment_tag(args)
+    print(tag)
+    submit(tag, cmd)
