fe16bdab594edc298622d6f181846628ed516b21
amal-ghamdi <amal.m.alghamdi@gmail.com>
2024-11-14 13:35:58 +0300

    pass u0 to create_args_list

--- code/ear_aqueducts/submit_all.py
+++ code/ear_aqueducts/submit_all.py
@@ -502,10 +502,11 @@ if version == "paperV2CARealDiff_CArbc_clip_grad_data":
     noise_levels = ["fromDataAvg"] # this noise level will not be used here
     NUTS_kwargs = {"max_depth":7}
     data_grad = True
+    u0_from_data = True
 
 # Main command to run the job
 main_command = "python advection_diffusion_inference.py"
-arg_list = create_args_list(animals, ears, noise_levels, num_ST_list, add_data_pts_list, unknown_par_types, unknown_par_values, data_type, version, sampler, Ns, Nb, inference_type, true_a, rbc, NUTS_kwargs, data_grad)
+arg_list = create_args_list(animals, ears, noise_levels, num_ST_list, add_data_pts_list, unknown_par_types, unknown_par_values, data_type, version, sampler, Ns, Nb, inference_type, true_a, rbc, NUTS_kwargs, data_grad, u0_from_data)
 print("length of arg_list: ", len(arg_list))
 for args in arg_list:
     cmd = create_command(main_command, args)
